d7c9172c094fdae138d1b2d6c5363f9c36205694
Mason Burke <masonburke@rocketmail.com>
2020-07-29 15:04:57 -0600

    carousel added to homepage

--- logic.js
+++ logic.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
+  // carousel 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
