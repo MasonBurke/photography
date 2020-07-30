0a25d505aeef779fec43f0b2e744fdb23e5b20c2
Mason Burke <masonburke@rocketmail.com>
2020-07-30 12:19:13 -0600

    rates page added

--- logic.js
+++ logic.js
@@ -14,6 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
   });
+// collapsable 
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
 
 
